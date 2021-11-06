32d37da7256a6d72f999ccfd81e8dc1ddf24035c
bs000sb <48981167+bs000sb@users.noreply.github.com>
2021-11-06 23:06:06 +0900

    added animation to robot

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -44,4 +44,14 @@ public class EnemyController : MonoBehaviour
 
         rigidbody2D.MovePosition(position);
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
 }
